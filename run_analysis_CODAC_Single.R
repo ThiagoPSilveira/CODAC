f862c18e1863bd9e863751d4229bf9d928c42ca9
deassis-lab <leonardo.deassis@cmb.gu.se>
2026-08-18 16:00:13 +0200

    Simplify comments regarding data file setup
    Removed redundant comment about CODAC_Results folder creation.

--- run_analysis_CODAC_Single.R
+++ run_analysis_CODAC_Single.R
@@ -37,12 +37,9 @@ remotes::install_github("ThiagoPSilveira/CODAC", subdir = "CODAC")
 # Tab-delimited file: genes in rows, samples in columns, gene name in column 1.
 data_path <- "C:/path/to/your/single_gene_data.txt"   # <-- EDIT THIS
 
-# Work in the folder that contains your data file. The engine automatically
-# creates a "CODAC_Results" folder here (same behavior as the PyCharm run),
-# so we do NOT create it in R -- otherwise you would get CODAC_Results/CODAC_Results.
+# Work in the folder that contains your data file. 
 setwd(dirname(data_path))
 results_path <- file.path(dirname(data_path), "CODAC_Results")
-
 
 # ------------------------------------------------------------------------------
 # 2. LOAD THE PACKAGE AND THE DATA
